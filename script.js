457e5c5795caf6ae7003b49341f969b23096c70c
unknown <sefereliaysu2@gmail.com>
2023-07-31 17:55:04 +0400

    loops

---
+++ script.js
@@ -0,0 +1,19 @@
+let s=1;
+
+for(let i=1; i<=5; i++) {
+    s*=i
+}
+console.log(s)
+
+
+
+let number = prompt('Eded daxil edin.')
+
+let m=0;
+
+for(let i=1; i<=number; i++) {
+    if(i%3==0) {
+        m+=i
+    }
+}
+console.log(m)
